355d3a0da3a7444c1b5acf950063ea9a65598c83
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-07-07 17:55:28 -0300

    chore: initialize StreamHub API project

---
+++ src/main/java/com/kayque/streamhubapi/StreamhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.kayque.streamhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamhubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamhubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kayque/streamhubapi/StreamhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayque.streamhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamhubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
